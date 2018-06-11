2dbfd1ffed8d2869f5016a25e9bc838c3a523dc0
maurizioterreni <maurizio.terreni@live.it>
2018-06-11 18:43:28 +0200

    bugfix

--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-06-11 11:33:41
+//This file has been generated on 2018-06-11 18:42:51
 
 #include "Arduino.h"
 #include "Arduino.h"
