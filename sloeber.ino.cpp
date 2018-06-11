a638f26fefe57323fceb6a247f087c51278a35d9
maurizioterreni <maurizio.terreni@live.it>
2018-06-11 11:35:01 +0200

    aggiornato sloeber

--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-06-10 19:06:38
+//This file has been generated on 2018-06-11 11:33:41
 
 #include "Arduino.h"
 #include "Arduino.h"
