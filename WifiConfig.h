7b46b32d8009646a49cf9d71f4ad73cf93a0840b
maurizioterreni <maurizio.terreni@live.it>
2018-06-17 01:20:19 +0200

    WifiConfig

---
+++ WifiConfig.h
@@ -0,0 +1,11 @@
+#ifndef Included_WifiConfig_H
+
+#define Included_WifiConfig_H
+
+char ssid[] = "Twim";            // your network SSID (name)
+char pass[] = "12345678";        // your network password
+
+
+char server[] = "www.maurizioterreni.altervista.org";
+
+#endif
